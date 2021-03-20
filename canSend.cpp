a7531fb91226f4f9cf7d37a74c662cceabc5fff0
AnisZoubaier <78211261+AnisZoubaier@users.noreply.github.com>
2021-03-20 22:30:51 +0100

    Add files via upload

---
+++ canSend.cpp
@@ -0,0 +1,15 @@
+
+#include "canSend.hpp"
+
+canSend::canSend(){
+
+std::cout << "CAN Sockets" << std::endl;
+}
+
+
+void canSend::sendBewertung(){
+
+std::cout << "CAN Sockets 1" << std::endl;
+}
+
+// canSend::sendBewertung(can_frame.can_id ,can_frame.data)
--- canSend.hpp
+++ canSend.hpp
@@ -4,12 +4,7 @@
 #include <string.h>
 #include <unistd.h>
 #include <iostream>
-#include <net/if.h>
-#include <sys/ioctl.h>
-#include <sys/socket.h>
 
-#include <linux/can.h>
-#include <linux/can/raw.h>
 
 class canSend
 {
@@ -19,8 +14,6 @@ public:
     //sendBewertung(can_frame.can_id ,can_frame.data);
 private:
     int s;
-	struct sockaddr_can addr;
-	struct ifreq ifr;
-	struct can_frame frame;
+
 
 };
